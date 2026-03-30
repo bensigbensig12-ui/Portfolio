76ba759b8d0b3ded8830ce0d314b2416fd464492
bensigbensig12-ui <bensigbensig12@gmail.com>
2026-03-30 09:47:51 +0800

    Zoomed out the BGvid

--- obj/Debug/net10.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11dbc0e9aa5cdedd2d80d64f993848a90ed01a98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067e357663ba4199e133c52b9d7290bf481844b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
